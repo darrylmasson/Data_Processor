6ee6df67048bfd4bea882f703cfa9e1b720e1478
Darryl <dmasson@purdue.edu>
2015-03-06 17:38:46 -0500

    Laplace method 1.0

---
+++ inc/LAP.h
@@ -0,0 +1,30 @@
+#ifndef LAP_H
+#define LAP_H
+
+#ifndef METHOD_H
+#include "Method.h"
+#endif
+
+class LAP : public Method {
+	private:
+		static unique_ptr<TTree> tree;
+		static int si_howmany;
+		static bool sb_initialized;
+		static double sd_laplace[8];
+		
+		unique_ptr<double[]> d_EXP;
+		double d_scale_v;
+		
+	public:
+		LAP(int ch, int len, double s);
+		virtual ~LAP();
+		virtual void evaluate(const shared_ptr<Event> event);
+		static void root_fill() {LAP::tree->Fill();}
+		static void root_init(TTree* tree_in);
+		static TTree* root_deinit() {return LAP::tree.release();}
+		static int HowMany() {return LAP::si_howmany;}
+		static float sf_version;
+		static float sf_s;
+};
+
+#endif // LAP_H
---
+++ src/LAP.cpp
@@ -0,0 +1,49 @@
+#include "LAP.h"
+#include <cmath>
+#include <iostream>
+
+float LAP::si_version = 1.0;
+float LAP::sf_s = 1.5;
+bool LAP::sb_initialized = false;
+unique_ptr<TTree> LAP::tree = nullptr;
+int LAP::si_howmany = 0;
+
+double LAP::laplace[8] = {0,0,0,0,0,0,0,0};
+
+LAP::LAP(int ch, int len, shared_ptr<Digitizer> digitizer) {
+	id = ch;
+	eventlength = len;
+	d_scale_v = digitizer->ScaleV();
+	try {EXP = unique_ptr<double[]>(new double[eventlength]);}
+	catch (bad_alloc& ba) {
+		failed |= alloc_error;
+		return;
+	}
+	for (int i = 0; i < eventlength; i++) d_EXP[i] = exp(-LAP::sf_s*digitizer->ScaleT()*i);
+	LAP::si_howmany++;
+}
+
+LAP::~LAP() {
+	if (g_verbose) cout << " LAP " << id << " d'tor ";
+	d_EXP.reset();
+	LAP::si_howmany--;
+}
+
+LAP::root_init(TTree* tree_in) {
+	if (!LAP::sb_initialized) {
+		LAP::tree = unique_ptr<TTree>(tree_in);
+		
+		LAP::tree->Branch("Laplace", LAP::laplace, "lap[8]/D");
+		
+		LAP::sb_initialized = true;
+	}
+}
+
+LAP::Evaluate(const shared_ptr<Event> event) {
+	int t(0);
+	LAP::laplace[id] = 0;
+	for (t = event->peak_x; t < eventlength; t++) {
+		LAP::laplace[id] += d_EXP[t - event->peak_x]*event->trace[t];
+	}
+	LAP::laplace[id] *= d_scale_v;
+}
